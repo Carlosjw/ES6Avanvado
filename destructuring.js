7abfc34f130b40ce04d5cabbc7823b7f0f1ce9ef
Carlosjw <carlos.ti.infor@gmail.com>
2021-08-10 09:20:03 -0300

    firstLessonDesctruture

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+// tradicional
+var arr = ["Apple", "Banana", "Orange", ["Tomato"]];
+
+var apple = arr[0];
+var banana = arr[1];
+var orange = arr[2];
+
+// Destructuring Assignment
+var [apple2, banana2, orange2, [tomato2]] = [
+  "Apple",
+  "Banana",
+  "Orange",
+  ["tomato"],
+];
+
+console.log(apple, apple2);
