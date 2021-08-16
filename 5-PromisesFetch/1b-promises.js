ada4e452ff640aa6e395e8e707fc9767d483b5a8
Carlos Lima <carlos.ti.infor@gmail.com>
2021-08-15 23:45:26 -0300

    PromisesAndCallbacks

---
+++ 5-PromisesFetch/1b-promises.js
@@ -0,0 +1,60 @@
+// Promises
+const doSomethingPromise = new Promise((resolve, reject) => {
+  throw new Error("Something went wrong");
+  setTimeout(function () {
+    // did doSomething
+    resolve("First data");
+  }, 1000);
+});
+
+const doOtherthingPromise = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    // did doSomething
+    resolve("Second data");
+  }, 1000);
+});
+
+doSomethingPromise.then((data) => console.log(data)).catch();
+
+// Callbacks
+function doSomething(callback) {
+  setTimeout(function () {
+    // did something
+    callback("First data");
+  }, 1000);
+}
+
+function doOtherThing(callback) {
+  setTimeout(function () {
+    // did other thing
+    callback("Second data");
+  }, 1000);
+}
+
+function doAll() {
+  try {
+    doSomething(function (data) {
+      var processedData = data.split("");
+
+      try {
+        doOtherThing(function (data2) {
+          var processedData2 = data2.split("");
+
+          try {
+            setTimeout(function () {
+              console.log(processedData, processedData2);
+            }, 1000);
+          } catch (err) {
+            // handle error
+          }
+        });
+      } catch (err) {
+        // handle error
+      }
+    });
+  } catch (err) {
+    // handle error
+  }
+}
+
+doAll();
